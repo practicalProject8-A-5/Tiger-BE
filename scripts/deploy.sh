990afd620e62b88ff4c3fd9f237893127560cfe0
tanibourne <tanibourne@gmail.com>
2022-09-28 23:00:35 +0900

    <feat> deletePastDate query 개선

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,8 +15,8 @@ echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> sudo kill -9 $CURRENT_PID"
-    sudo kill -9 $CURRENT_PID
+    echo "> sudo kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
     sleep 5
 fi
 
